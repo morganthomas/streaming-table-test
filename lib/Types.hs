b763f1706e8a0415700768263301c1e38fa87b6f
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2020-09-18 01:17:25 -0600

    server works

--- lib/Types.hs
+++ lib/Types.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
+
+
 module Types where
 
 
+import Data.Aeson
 import Data.CountryCodes
 import Data.Text
+import Servant
 import Test.QuickCheck
 
 import StockName
@@ -11,6 +17,15 @@ import StockName
 data Sex = Male | Female
   deriving (Eq, Ord, Show)
 
+instance ToJSON Sex where
+  toJSON Male = toJSON @Text "male"
+  toJSON Female = toJSON @Text "female"
+
+instance FromJSON Sex where
+  parseJSON (String "male") = return Male
+  parseJSON (String "female") = return Female
+  parseJSON _ = fail "not a valid sex"
+
 
 data Person = Person
             { name   :: Text
@@ -18,6 +33,20 @@ data Person = Person
             , sex    :: Sex
             , origin :: CountryCode }
   deriving (Eq, Show)
+
+instance ToJSON Person where
+  toJSON p = object [ "name" .= name p
+                    , "age" .= age p
+                    , "sex" .= sex p
+                    , "origin" .= origin p ]
+
+instance FromJSON Person where
+  parseJSON = withObject "Person" $ \p -> do
+    n <- p .: "name"
+    a <- p .: "age"
+    s <- p .: "sex"
+    o <- p .: "origin"
+    return (Person n a s o)
 
 
 instance Arbitrary Sex where
@@ -30,3 +59,6 @@ instance Arbitrary CountryCode where
 
 instance Arbitrary Person where
   arbitrary = Person <$> (unStockName <$> arbitrary) <*> choose (0,120) <*> arbitrary <*> arbitrary
+
+
+type Api = StreamGet NewlineFraming JSON (SourceIO Person)
--- server/Main.hs
+++ server/Main.hs
@@ -1,4 +1,46 @@
 module Server where
 
+
+import Data.Functor.Of
+import Data.Proxy
+import Network.Wai.Handler.Warp
+import Servant hiding (Stream)
+import Servant.Types.SourceT (SourceT (..), StepT (..))
+import Streaming (Stream)
+import qualified Streaming
+import qualified Streaming.Prelude as Streaming
+import Test.QuickCheck
+
+import Types
+
+
+numPeople :: Int
+numPeople = 10000
+
+
+arbitraryPerson :: IO Person
+arbitraryPerson = generate arbitrary
+
+
+peopleStream :: Stream (Of Person) IO ()
+peopleStream = Streaming.replicateM numPeople arbitraryPerson
+
+
+streamSource :: Stream (Of a) IO () -> SourceIO a
+streamSource s = SourceT $ \f -> f (Streaming.destroy s (\(x :> xs) -> Yield x xs) Effect (const Stop))
+
+
+streamingPeople :: Handler (SourceIO Person)
+streamingPeople = return $ streamSource peopleStream
+
+
+server :: Server Api
+server = streamingPeople
+
+
+api :: Proxy Api
+api = Proxy
+
+
 main :: IO ()
-main = putStrLn "hello world"
+main = run 8081 . serve api $ server
